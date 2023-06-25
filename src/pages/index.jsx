9654a4c9ce37e898600f0cd4a281bb5b2e82832c
John Dave Manuel <jdmanuel242@gmail.com>
2023-06-25 10:55:00 +0800

    chore: move other pages

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -410,52 +410,6 @@ export default function Home({ articles }) {
           </div>
         </div>
       </Container>
-	  {/* PROJECTS */}
-	  <SimpleLayout
-        title="Throughout my career, I have had the opportunity to work on a diverse range of projects, each presenting unique challenges and opportunities for growth."
-        intro=" I have been fortunate to contribute to the success of several notable initiatives, and I am proud to share my experience with you."
-      >
-        <ul
-          role="list"
-          className="grid grid-cols-1 gap-x-12 gap-y-16 sm:grid-cols-2 lg:grid-cols-3"
-        >
-          {projects.map((project) => (
-            <Card as="li" key={project.name}>
-              <div className="relative z-10 flex h-12 w-12 items-center justify-center rounded-full bg-white shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0">
-                <Image
-                  src={project.logo}
-                  alt=""
-                  className="h-8 w-8"
-                  unoptimized
-                />
-              </div>
-              <h2 className="mt-6 text-base font-semibold text-zinc-800 dark:text-zinc-100">
-                <Card.Link href={project.link.href}>{project.name}</Card.Link>
-              </h2>
-              <Card.Description>{project.description}</Card.Description>
-              <p className="relative z-10 mt-6 flex text-sm font-medium text-zinc-400 transition group-hover:text-teal-500 dark:text-zinc-200">
-                <LinkIcon className="h-6 w-6 flex-none" />
-                <span className="ml-2">{project.link.label}</span>
-              </p>
-            </Card>
-          ))}
-        </ul>
-      </SimpleLayout>
-
-      <Container className="mt-24 md:mt-28">
-        <div className="mx-auto grid max-w-xl grid-cols-1 gap-y-20 lg:max-w-none lg:grid-cols-2">
-          <div className="flex flex-col gap-16">
-		  {/* <Tech /> */}
-          </div>
-          <div className="space-y-10 lg:pl-16 xl:pl-24">
-            {/* <Tech /> */}
-          </div>
-        </div>
-      </Container>
-
-	  <Container className="mt-24 md:mt-28">
-		<Resume/>
-      </Container>
     </>
   )
 }
